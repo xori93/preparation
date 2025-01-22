8a8f1603b13bd507fee58e76152568be9686d20f
Xori <xori93@aol.com>
2025-01-22 10:08:43 -0500

    working on click event

---
+++ index.js
@@ -0,0 +1,35 @@
+// API 1: "https://jsonplaceholder.typicode.com/users"
+// API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main () {
+  // fetch data.
+  // store data in a variable.
+  // wait for the fetch.
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  // convert backend to frontend and wait for it.
+  const usersData = await users.json()
+  const userListEl = document.querySelector(".user-list")
+
+  // turn data object to html. So lets map it
+ userListEl.innerHTML = usersData.map(
+    (user) => userHTML(user)).join("")
+}
+
+main()
+
+function showerUserPosts(id) {
+  console.log(id)
+}
+
+function userHTML(user) {
+  return `<div class="user-card" onclick="showUserPosts(${user.id})">
+  <div class="user-card__container">
+    <h3>${user.name}</h4>
+      <p><b>Email:</b> ${user.email}</p>
+      <p><b>Phone:</b> ${user.phone}</p>
+      <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
+      ${user.website}
+      </a></p>
+  </div>
+</div>`
+}
